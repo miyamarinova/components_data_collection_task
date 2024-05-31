c319532a0f1e9f43d45f20a74fe93aa6984dc994
miyamarinova <miyamarinova@gmail.com>
2024-05-31 05:52:55 -0400

    Upgrade pipelines

--- components/items.py
+++ components/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class ComponentsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    processors = scrapy.Field()
+    gpus = scrapy.Field()
+    motherboards = scrapy.Field()
+    rams = scrapy.Field()
--- components/pipelines.py
+++ components/pipelines.py
@@ -1,13 +1,38 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class ComponentsPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect('components.db')
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS components(
+        processor TEXT,
+        gpu TEXT,
+        motherboard TEXT,
+        ram TEXT
+        )""")
+
     def process_item(self, item, spider):
+        processors = item.get('processors', [])
+        gpus = item.get('gpus', [])
+        motherboards = item.get('motherboards', [])
+        rams = item.get('rams', [])
+
+        max_length = max(len(processors), len(gpus), len(motherboards), len(rams))
+        processors.extend([''] * (max_length - len(processors)))
+        gpus.extend([''] * (max_length - len(gpus)))
+        motherboards.extend([''] * (max_length - len(motherboards)))
+        rams.extend([''] * (max_length - len(rams)))
+
+        for i in range(max_length):
+            self.cur.execute("INSERT INTO components  VALUES (?, ?, ?, ?)",
+                             (processors[i],
+                              gpus[i],
+                              motherboards[i],
+                              rams[i]))
+
+        self.con.commit()
         return item
